183063a2a97dff90e602e6cefd5869093102fbc0
rizwanahmed1981 <109456571+rizwanahmed1981@users.noreply.github.com>
2023-12-03 15:36:51 +0500

    ninety-percent

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+
+
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+    title: 'Customer',
+  };
+
 export default function Customer(){
     return(
         <h2>Customer</h2>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
+import { Metadata } from 'next';
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+export const metadata: Metadata = {
+  title:  {
+    template: '%s | Malaika Dashboard',
+    default: 'Malaika Dashboard',
+  },
+  description: 'The official Malaika Dashboard, built with Next.js.',
+  metadataBase: new URL('https://malaika-dashboard.vercel.app/'),
+};
+ 
 export default function RootLayout({
   children,
 }: {
